ee4118421a3a715d4846f4987a721cdbb23100d2
vanshsoni2003 <138280482+vanshsoni2003@users.noreply.github.com>
2025-08-06 18:32:06 +0530

    perfume web site react.js

---
+++ postcss.config.cjs
@@ -0,0 +1,10 @@
+// postcss.config.cjs
+
+const tailwindcss = require('@tailwindcss/postcss');
+
+module.exports = {
+  plugins: [
+    tailwindcss,
+    require('autoprefixer'),
+  ],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/perfume-project/',  // 🔁 Replace with your GitHub repo name
-  plugins: [tailwindcss(), react()],
-=======
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
->>>>>>> e544bd2b2daa1fcd10290220b9bfca9ed25f538a
+  base: '/perfume-project/',  // ✅ MUST match the GitHub repo name
+  plugins: [react()],
 })
